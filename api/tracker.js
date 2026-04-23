76513610afe958dd55044ca34f3d8a8bd3d106e5
thepenguin0921 <166737388+thepenguin0921@users.noreply.github.com>
2026-04-23 17:55:02 +0800

    Update tracker.js

--- api/tracker.js
+++ api/tracker.js
@@ -1,15 +1,3 @@
-const data = {};
-
 export default function handler(req, res) {
-  const { app, action } = req.query;
-  
-  if (app && action) {
-    if (!data[app]) data[app] = { opens: 0, time: 0, last: null };
-    if (action === 'open') {
-      data[app].opens++;
-      data[app].last = new Date().toISOString();
-    }
-  }
-  
-  res.json(data);
+  res.status(200).json({ status: "ok" });
 }
