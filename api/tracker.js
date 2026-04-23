fc596f8f6d6c75a09992830620e9c2fa958ff89d
thepenguin0921 <166737388+thepenguin0921@users.noreply.github.com>
2026-04-23 18:44:19 +0800

    Update tracker.js

--- api/tracker.js
+++ api/tracker.js
@@ -1,5 +1,7 @@
-const SUPABASE_URL = https://abjpteijsoohyjaztmlm.supabase.co;
-const SUPABASE_KEY = sb_publishable_cfwT4OSVZu0xpIy2xplL7A_o2acrnRu;
+import https from 'https';
+
+const SUPABASE_URL = 'https://abjpteijsoohyjaztmlm.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_cfwT4OSVZu0xpIy2xplL7A_o2acrnRu';
 
 export default async function handler(req, res) {
   const { app, action } = req.query;
